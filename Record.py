c39a64ec052143316d9b459843fb8a0840c93395
Peter Thatcher <pthatcher@gmail.com>
2009-10-22 14:01:51 -0700

    add Record.py

---
+++ Record.py
@@ -0,0 +1,150 @@
+import operator
+from itertools import islice
+
+def Record(*slots):
+    class cls(RecordBase):
+        pass
+
+    cls.setSlots(slots)
+
+    return cls
+
+class RecordBase(tuple):
+    SLOTS = ()
+
+    ## overide this one instead of __init__
+    def __new__(cls, *values):
+        assert len(values) == len(cls.SLOTS)
+        return tuple.__new__(cls, values)
+
+    ## use this when overriding __new__
+    @classmethod
+    def new(cls, *values):
+        assert len(values) == len(cls.SLOTS)
+        return tuple.__new__(cls, values)
+
+    def __iter__(self):
+        return islice(tuple.__iter__(self), len(self.SLOTS))
+
+    ## use this when overriding __iter__
+    @property
+    def values(self):
+        return islice(tuple.__iter__(self), len(self.SLOTS))
+
+
+    def __repr__(self):
+        return self.__class__.__name__ + repr(tuple(self.values))
+
+    def alter(self, **value_by_slot):
+        return self.new(*(value_by_slot.get(self.SLOTS[index], value)
+                          for index, value
+                          in enumerate(self.values)))
+
+    def getByIndex(self, index):
+        return tuple.__getitem__(self, index)
+
+    def setByIndex(self, set_index, set_value):
+        return self.new(*(set_value if set_index == index else value
+                          for (index, value)
+                          in enumerate(self.values)))
+
+
+    ## setting up getters and setters
+    @classmethod
+    def setSlots(cls, slots):
+        cls.SLOTS = slots
+        for index, slot in enumerate(slots):
+            setattr(cls, slot,                      property(cls.makeGetter(index)))
+            setattr(cls, "set" + upper_first(slot), cls.makeSetter(index))
+
+    @classmethod
+    def makeGetter(cls, index):
+        return lambda self : self.getByIndex(index)
+
+    @classmethod
+    def makeSetter(cls, index):
+        return lambda self, value: self.setByIndex(index, value)
+
+    ## for Pickling
+
+    def __getnewargs__(self):
+        return self
+
+def upper_first(string):
+    return string[0].upper() + string[1:]
+    
+
+if __name__ == "__main__":
+    # tests
+    def err(code, err_type = Exception):
+        try:
+            code()
+        except err_type, error:
+            return error
+        
+    class Person(Record("name", "age")):
+        pass
+
+    name   = "peter"
+    age    = 25
+
+    peter  = Person(name, age)
+    assert peter.name          == name 
+    assert peter[0]            == name 
+    assert peter.age           == age
+    assert peter[1]            == age
+    assert tuple(peter.values) == (name, age)
+    assert tuple(peter)        == (name, age)
+    assert peter               == Person("peter", 25)
+    assert repr(peter)         == "Person('peter', 25)"
+    assert err(lambda : peter.height, AttributeError)
+    assert err(lambda : peter[2],     IndexError)
+    assert err(lambda : Person())
+    assert err(lambda : Person("peter"))
+
+    assert peter.alter(age = 29) == Person(name, 29)
+    assert peter.setAge(29)      == Person(name, 29)
+    assert peter.alter(name = "grandpa", age = 99) == Person("grandpa", 99)
+    assert peter.setName("grandpa").setAge(99)     == Person("grandpa", 99)
+
+    def iget(itr, index):
+        try:
+            itr = iter(itr)
+            for _ in xrange(index):
+                itr.next()
+            return itr.next()
+        except StopIteration:
+            raise IndexError
+    
+    class LinkedList(Record("first", "rest")):
+        def __new__(cls, first, rest = None):
+            return cls.new(first, rest)
+
+        def __getitem__(self, index):
+            return iget(self, index)
+
+        def __iter__(rest):
+            while True:
+                first, rest = rest.values
+                yield first
+                if rest is None:
+                    raise StopIteration
+
+        def cons(self, value):
+            return self.new(value, self)
+
+                    
+    names1  = LinkedList("alice")
+    names2  = names1.cons("bob")
+    names2z = names1.cons("zed")
+    names3  = names2.cons("charlie")
+
+    assert list(names1)  == ["alice"]
+    assert list(names2)  == ["bob", "alice"]
+    assert list(names2z) == ["zed", "alice"]
+    assert list(names3)  == ["charlie", "bob", "alice"]
+    assert names3[1]     == "bob"
+    assert names3[0]     == "charlie"
+    assert err(lambda : names3[3], IndexError)
+
+    assert (names3.first, list(names3.rest)) == ("charlie", ["bob", "alice"])
